d7042549b9b4e81763e0b58c653b2a83dfbb13b3
Vitor Markis <vitormarkis2369@gmail.com>
2023-03-14 21:23:00 -0300

    first satisfactory result

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,19 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { Inter } from 'next/font/google'
 
 import "~/styles/globals.css";
 
+const inter = Inter({
+  weight: ['400', "500", "600", "700", "800", "900"],
+  subsets: ['latin'],
+})
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 };
 
 export default MyApp;
