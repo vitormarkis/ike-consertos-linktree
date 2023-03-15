5da78af4d692dcdca0e5978b591583687581c423
Vitor Markis <vitormarkis2369@gmail.com>
2023-03-15 03:40:52 -0300

    redesign

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,26 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
+// import localFont from "next/font/local";
+
+// const tesla = localFont({
+//   src: [
+//     {
+//       path: "../assets/TESLA.ttf",
+//     },
+//   ],
+//   variable: "--font-tesla",
+// });
 
 import "~/styles/globals.css";
 
 const inter = Inter({
-  weight: ['400', "500", "600", "700", "800", "900"],
-  subsets: ['latin'],
-})
+  weight: ["400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <main className={inter.className}>
+    <main className={`${inter.className}`}>
       <Component {...pageProps} />
     </main>
   );
